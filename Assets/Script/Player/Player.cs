a32e35c62bf9865b7849e5783e80ff068c83129d
Doggo <382782974@qq.com>
2022-09-16 10:14:12 +0800

    上传工程

---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private float InputX;
+    private float InputY;
+    private Vector2 MovementInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        PlayerInput();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    private void PlayerInput()
+    {
+        InputX = Input.GetAxisRaw("Horizontal");
+        InputY = Input.GetAxisRaw("Vertical");
+        if (InputX != 0 && InputY != 0)
+        {
+            InputX *= 0.6f;
+            InputY *= 0.6f;
+        }
+        MovementInput = new Vector2(InputX, InputY);
+    }
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + MovementInput * speed * Time.deltaTime);
+    }
+}
