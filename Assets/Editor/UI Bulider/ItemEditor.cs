70e6c5ee7a506f65297ae0f58103c0a2a42e413b
wangke <382782974@qq.com>
2022-09-21 12:28:04 +0800

    UI Toolkit工具编写

---
+++ Assets/Editor/UI Bulider/ItemEditor.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class ItemEditor : EditorWindow
+{
+    [MenuItem("Doggo/ItemEditor")]
+    public static void ShowExample()
+    {
+        ItemEditor wnd = GetWindow<ItemEditor>();
+        wnd.titleContent = new GUIContent("ItemEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+/*        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);*/
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UI Bulider/ItemEditor.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        root.Add(labelFromUXML);
+
+        // A stylesheet can be added to a VisualElement.
+        // The style will be applied to the VisualElement and all of its children.
+/*        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/UI Bulider/ItemEditor.uss");
+        VisualElement labelWithStyle = new Label("Hello World! With Style");
+        labelWithStyle.styleSheets.Add(styleSheet);
+        root.Add(labelWithStyle);*/
+    }
+}
