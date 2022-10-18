aa6bd16176291ee42c7d523ab5f209a2ad8bb60c
wangke <382782974@qq.com>
2022-10-18 11:40:41 +0800

    使用动画状态机的层级树来为人物制作走路跑步动画

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -9,14 +9,18 @@ public class Player : MonoBehaviour //控制玩家基本操作的类
     private float InputX;
     private float InputY;
     private Vector2 MovementInput;
+    private Animator[] animators;
+    private bool isMoving;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animators = GetComponentsInChildren<Animator>();
     }
     private void Update()
     {
         PlayerInput();
+        SwitchAnimator();
     }
     private void FixedUpdate()
     {
@@ -31,10 +35,29 @@ public class Player : MonoBehaviour //控制玩家基本操作的类
             InputX *= 0.6f;
             InputY *= 0.6f;
         }
+        //走路状态速度
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            InputX *= 0.5f;
+            InputY *= 0.5f;
+        }
         MovementInput = new Vector2(InputX, InputY);
+        isMoving = MovementInput != Vector2.zero;
     }
     private void Movement()
     {
         rb.MovePosition(rb.position + MovementInput * speed * Time.deltaTime);
+    }
+    private void SwitchAnimator()
+    {
+        foreach (var anim in animators)
+        {
+            anim.SetBool("isMoving", isMoving);
+            if (isMoving)
+            {
+                anim.SetFloat("InputX", InputX);
+                anim.SetFloat("InputY", InputY);
+            }
+        }
     }
 }
