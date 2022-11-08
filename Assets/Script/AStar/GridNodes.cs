72ca18b82e9f4674485f3854c34f0e9f30dcb028
wangke <382782974@qq.com>
2022-11-08 11:10:45 +0800

    AStar算法基础数据数据结构构建

---
+++ Assets/Script/AStar/GridNodes.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//管理Grid中所有网格节点
+namespace MFarm.AStar
+{
+    public class GridNodes
+    {
+        //地图整体的宽和高
+        private int width;
+        private int height;
+        private Node[,] gridNode;//二维数组来保存地图中的节点
+        /// <summary>
+        /// 构造函数初始化节点范围数组
+        /// </summary>
+        /// <param name="width">地图宽度</param>
+        /// <param name="height">地图高度</param>
+        public GridNodes(int width, int height)//构造函数,初始化
+        {
+            this.width = width;
+            this.height = height;
+            //手动将节点装入数组
+            gridNode = new Node[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    gridNode[x, y] = new Node(new Vector2Int(x, y));//new Node时会自动调用Node中的构造函数将不同的x,y值传递给每一个节点
+                }
+            }
+        }
+        /// <summary>
+        /// 根据x,y返回一个节点
+        /// </summary>
+        /// <param name="xPos">横坐标</param>
+        /// <param name="yPos">纵坐标</param>
+        /// <returns></returns>
+        public Node GetGridNode(int xPos,int yPos)
+        {
+            if (xPos < width && yPos < height)//确保该坐标在地图范围之内
+            {
+                return gridNode[xPos, yPos];
+            }
+            else
+            {
+                Debug.Log("超出网格范围!");
+                return null;
+            }
+        }
+    }
+}
+
---
+++ Assets/Script/AStar/Node.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+//AStar算法,计算最短节点路径
+namespace MFarm.AStar
+{
+    public class Node : IComparable<Node>//Unity自带的用作比较的接口
+    {
+        public Vector2Int gridPosition;//节点的坐标
+
+        public int gCost = 0;//距离起点(Start)格子的距离
+
+        public int hCost = 0;//距离终点(Target)格子的距离
+
+        public int FCost => gCost + hCost;//当前格子的权重
+
+        public bool isObstacle = false;//判断当前格子是否是障碍
+
+        public Node parentNode;//当前节点的父节点
+
+        public Node(Vector2Int pos)//构造函数,当前节点的初始化
+        {
+            gridPosition = pos;
+            parentNode = null;
+        }
+
+        /// <summary>
+        /// Unity自带的比较接口中的函数方法
+        /// </summary>
+        /// <param name="other">所要比较的另外一个节点</param>
+        /// <returns>大于返回1,相等返回0,小于返回-1</returns>
+        public int CompareTo(Node other)
+        {
+            int result = FCost.CompareTo(other.FCost);//比较出相邻格子中最小的权重,返回1,0,-1
+            if (result == 0)//如果权重相等
+            {
+                result = hCost.CompareTo(other.hCost);//则比较格子的hCost(距离终点格子的距离)
+            }
+            return result;
+        }
+    }
+}
+
